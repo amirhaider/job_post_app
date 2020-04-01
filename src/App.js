5a7d8b3c16ecd26e5625e913912c2646a86169ef
amirhaider <haider@trimulabs.com>
2020-04-02 00:57:13 +0500

    style ui

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import Job from "./Components/Jobs";
 import Header from './Components/header';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './style/base/_settings.css';
+import './style/base/base.css';
+import './style/component/jobs.css';
 const client = new ApolloClient({
   uri: "https://api.graphql.jobs/"
 });
@@ -12,7 +15,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
        <Router>
-        <div >
+        <div className="main_container" >
         <Header/>
           <Switch>
             <Route path="/"   component={Job} exact={true}/>
--- src/Components/Jobs.js
+++ src/Components/Jobs.js
@@ -6,7 +6,8 @@ import "../style/component/jobs.css";
 
 
 
-// GQ Query to fetch all the JOBS
+
+
 const JOBS = gql`
   {
     jobs {
@@ -17,6 +18,7 @@ const JOBS = gql`
       slug
       company {
         name
+        slug
       }
       userEmail
       applyUrl
@@ -27,45 +29,99 @@ const JOBS = gql`
 export default function Job(props) {
   
   let [jobs, setJobs] = useState([]);
+  let [filter, setfilter] = useState([]);
   const { loading, data } = useQuery(JOBS);
   useEffect(() => {
     if(data){
     
     setJobs(data.jobs);
+    setfilter(data.jobs);
   }
     
     
   }, [data])
+  const onChange = e => {
+    let f = jobs.filter(
+      f =>
+        f.title.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        f.company.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setfilter(f);
+  };
   if (loading)
     return (
      <div className="loading">
      <h1>loading....</h1>
       </div>
     );
-  
- 
-
   return (
    <div>
     <input
             
             type="text"
             className="form"
-            placeholder="Enter Job or Company"
-            //onChange={onChange}
+            placeholder="Enter Job or Company for serch"
+            onChange={onChange}
           />
           
      
-     {jobs.map(function(item, i){
-       console.log(item,'index i',i);
-  return <div className="data">
-  <li key={item.id}>{item && item.id}</li>
-  <li key={item.id}>{item && item.item}</li>
-  <li key={item.id}>{item && item.isPublished}</li>
-  <li key={item.id}>{item && item.description}</li>
-  <li key={item.id}>{item && item.company.name}</li>
-  </div>
+     {filter.map(function(item, i){
+       console.log(item,i);
+  return <div className="main_div">
+  <div className="main_data_div">
+  <div className="data">
+  <input
+                        type="checkbox"
+                        className="custom_checkbox"
+                        id={item.id}
+                        checked={item.isPublished}
+                      />
+                      <label
+                        class="label"
+                        for={item.id}
+                        title="Published"
+                        
+                      ></label>
+                      {item.company && (
+                        <a
+                          href={`/jobs/${item.slug}/${item.company.slug}`}
+                          class="title"
+                        >
+                          {item.title}
+                        </a>
+                        
+                      )}
+                      <small>at </small>{" "}
+                      <b className="company_name">{item.company.name}</b>
+                      <p class="apply_view">
+                      <button
+                        onClick={item.applyUrl}
+                        className="apply"
+                      >
+                        Apply
+                      </button>
+                      {item.company && (
+                        <button
+                          onClick={`/jobs/${item.slug}/${item.company.slug}`}
+                          className="view_details"
+                          
+                        >
+                          Details
+                        </button>
+                      )}
+                    </p>
+                      <p className="description">{item.description}</p>
+                      {item.userEmail && (
+                      <p className="">
+                        <strong>Posted By:</strong>{" "}
+                        <a href={item.userEmail} className="email"> {item.userEmail}</a>
+                      </p>
+                    )}
+           </div>
+        </div>
+    </div>
 })}
    </div>
+  
   );
 }
